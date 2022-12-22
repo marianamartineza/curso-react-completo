55824f4f8720b1ec8038ec25acd8c258a97574ef
Mariana Martinez Araque <marianadelos.martinez@unet.edu.ve>
2022-12-22 13:20:53 -0400

    video 76. Agregar una nueva categoria
    tarea agregar una categoria en la nueva aplicacion de react, gifExpertApp - seccion 7 del curso de react

---
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -0,0 +1,41 @@
+import {useState} from 'react';
+
+export const GifExpertApp = () => {
+  
+  const [categories, setCategories] = useState(['One Punch', 'Dragon Ball']);
+
+  
+  const onAddCategory = () => {
+
+    // categories.push('Valorant');
+    // console.log('Valorant');
+    // setCategories([ ...categories, 'Valorant']);
+
+    setCategories([ 'Valorant', ...categories]);
+    
+  }
+
+  console.log(categories)
+  
+    return (
+   <>
+        {/* titulo */}
+        <h1>GiftExpertApp</h1>
+
+        {/* input */}
+
+        {/* listado de Gif */}
+        <button onClick={onAddCategory}>Agregar</button>
+
+        <ol> 
+            {
+                categories.map(category => {
+                    return <li key={category}>{category}</li>
+                })
+            }
+        </ol>
+           
+            {/* gif item */}
+   </>
+  )
+}
