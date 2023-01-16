045e3c9dfbaefc87be3478b2a48629422fc7b3bd
Mariana Martinez Araque <marianadelos.martinez@unet.edu.ve>
2023-01-16 15:23:46 -0400

    Video 103. Pruebas del componente GifGridItem
    saque la url y el titulo del test del snapshot y lo coloque para todas las pruebas del gifItem, se hicieron dos test mas el de verificar que la imagen reciba correctamente la url y el titulo. Y que el titulo se muestre como texto en el componente

--- 04-gif-expert-app/tests/components/GifItem.test.jsx
+++ 04-gif-expert-app/tests/components/GifItem.test.jsx
@@ -2,14 +2,34 @@ import { render,screen } from '@testing-library/react';
 import { GifItem } from '../../src/components';
 
 describe('Pruebas en <GifItem />', () => { 
+
+    const title='Este es el titulo';
+    const url='https://google.com.ve/img.jpg';
     
     test('debe de hacer match con el snapshot', () => { 
-        const title='Este es el titulo';
-        const url='https://google.com.ve';
 
         const {container} = render(<GifItem title={title} url={url}/>)
-        screen.debug();
+        // screen.debug();
         expect(container).toMatchSnapshot();
-     })
+     });
 
- })
+     
+     test('debe de recibir el url y el titulo correctamente', () => { 
+        
+       render(<GifItem title={title} url={url}/>)
+
+    //    expect(screen.getByRole('img').src).toBe(url);
+       const {src, alt} = screen.getByRole('img');
+       expect(src).toBe(url);
+       expect(alt).toBe(title);
+      });
+
+      test('debe de mostrar el titulo en el componenente', () => { 
+
+        render(<GifItem title={title} url={url}/>)
+        expect(screen.getByText(title)).toBeTruthy();
+
+       });
+
+
+ });
