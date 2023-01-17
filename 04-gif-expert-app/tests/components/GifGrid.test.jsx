dca18d5675729937387057e6de8642f616a221d2
Mariana Martinez Araque <marianadelos.martinez@unet.edu.ve>
2023-01-17 16:52:55 -0400

    video 109. Hacer un mock completo de un Custom Hooks
    con el jest.mock(ruta del hook), se puede probar cualquier hook bien sea de nuestra libreria personal o de un tercero.
    
    luego se usa el mockReturnValue para simular los valores que se deberian de obtener como resultado del hook.
    
    para poder evaluar el hook de los gifs se necesita simular un arreglo de datos que seran los gifs de prueba.
    
    se debe usar getAllByRole cuando se espera obtener mas de un solo elemento como resultado

--- 04-gif-expert-app/tests/components/GifGrid.test.jsx
+++ 04-gif-expert-app/tests/components/GifGrid.test.jsx
@@ -1,11 +1,19 @@
 import { render, screen } from "@testing-library/react"
 import { GifGrid } from "../../src/components"
+import { useFetchGifs } from '../../src/hooks/useFetchGifs';
+
+jest.mock('../../src/hooks/useFetchGifs')
 
 describe('pruebas en <GifGrid/>', () => { 
 
     const category = 'One Punch';
     
     test('debe de mostrar el loading inicialmente', () => { 
+
+        useFetchGifs.mockReturnValue({
+            images: [],
+            isLoading: true,
+        });
         
         // pruebas para evaluar el estado inicial del componente
         render(<GifGrid category={category}/>);
@@ -15,8 +23,30 @@ describe('pruebas en <GifGrid/>', () => {
      });
 
      test('debe de mostrar items cuando se cargan las imagenes mediante el useFetchGifs', () => { 
-        
 
+        const gifs = [
+            {
+                id:'ABC',
+                title: 'Saitama',
+                url: 'https://localhost/saitama.jpg',
+            },
+            {
+                id:'123',
+                title: 'Goku',
+                url: 'https://localhost/goku.jpg',
+            }
+        ];
+
+        useFetchGifs.mockReturnValue({
+            images: gifs,
+            isLoading: false,
+        });
+        
+        render(<GifGrid category={category}/>);
+
+        screen.debug();
+
+        expect(screen.getAllByRole('img').length).toBe(2);
 
       });
  })
