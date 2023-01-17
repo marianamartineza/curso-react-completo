ba636972cc4a2bd3e23254d33e11bb67d4b8c4c2
Mariana Martinez Araque <marianadelos.martinez@unet.edu.ve>
2023-01-17 14:50:09 -0400

    Update AddCategory.test.jsx
    fireEvent para activar el input y verificar su correcto funcionamiento

--- 04-gif-expert-app/tests/components/AddCategory.test.jsx
+++ 04-gif-expert-app/tests/components/AddCategory.test.jsx
@@ -1,11 +1,14 @@
-import { render,screen } from "@testing-library/react";
+import { fireEvent, render,screen } from "@testing-library/react";
 import { AddCategory } from "../../src/components";
 
 describe('Pruebas en <AddCategory />', () => { 
     
     test('debe de cambiar el valor de la caja de texto', () => { 
         
-        render(<AddCategory onNewCategory={ () => {}}/>)
+        render(<AddCategory onNewCategory={ () => {}}/>);
+        const input = screen.getByRole('textbox');
+        fireEvent.input( input, {target: { value: 'Saitama'}});
+        expect( input.value ).toBe('Saitama');
         screen.debug();
 
      });
