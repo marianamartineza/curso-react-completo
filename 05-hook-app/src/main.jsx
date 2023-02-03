6335f5a3d0b82239c11941b4f9cacb7f99d46e8d
Mariana Martinez Araque <marianadelos.martinez@unet.edu.ve>
2023-02-03 16:01:06 -0400

    video 123. Formulario con custom Hook
    dejar toda la logica en un hook externo es mas comodo y hace que el codigo quede mas limpio.
    
    aparte el useForm permite que se utilice con cualquier formulario sin importar que inputs tenga.

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { SimpleForm } from "./02-useEffect/SimpleForm";
-// import { CounterWithCustomHook } from "./01-useState/CounterWithCustomHook";
-// import { CounterApp } from "./01-useState/CounterApp";
+
 // import { HooksApp } from "./HooksApp";
+// import { CounterApp } from "./01-useState/CounterApp";
+// import { CounterWithCustomHook } from "./01-useState/CounterWithCustomHook";
+// import { SimpleForm } from "./02-useEffect/SimpleForm";
+import { FormWithCustomHook } from "./02-useEffect/FormWithCustomHook";
+
+
+
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <SimpleForm/>
-  </React.StrictMode>
+  // <React.StrictMode>
+    <FormWithCustomHook/>
+  //</React.StrictMode> 
 );
  
