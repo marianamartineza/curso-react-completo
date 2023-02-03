55ff7e9761fd938cabe5073dec6d429a5a206bd3
Mariana Martinez Araque <marianadelos.martinez@unet.edu.ve>
2023-02-03 12:09:25 -0400

    video 118 y 119. useEffect - simpleForm
    En el video 118 basicamente se Expusieron los metodos del customHook, creando funciones dentro del hook y devolviendolo en el objeto junto con el counter.
    
    El video 119 se creo el SimpleForm.jsx configurando un formulario controlado con useState que contiene el correo y nombre de usuario.
    
    Y con la funcion OnInputChange se obtiene el target del evento para poder desestructurar el name y el value.
    
    Y con el setFormState se extiende las propiedades del estado y se modifica el value segun el name del input.

---
+++ 05-hook-app/src/02-useEffect/SimpleForm.jsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+
+
+export const SimpleForm = () => {
+
+    const [formState, setFormState] = useState({
+        username: 'strider',
+        email: 'm@gmail.com',
+    });
+
+    const {username,email} = formState;
+
+    const onInputChange = ({target}) => {
+        const {name, value} = target;
+        // console.log({name, value});
+        setFormState({
+            ...formState,
+            [name]: value,
+        });
+    }
+
+    useEffect( () => {
+        console.log('useEffect called!');
+    });
+
+    return (
+        <>
+            <h1>Simple Form</h1>
+            <hr />
+
+            <input 
+                type="text"
+                className="form-control"
+                placeholder="Username"
+                name="username"
+                value={username}
+                onChange={onInputChange}
+            />
+
+            <input 
+                type="email"
+                className="form-control mt-2"
+                placeholder="marianamartinezaraque2021@gmail.com"
+                name="email"
+                value={email}
+                onChange={onInputChange}
+            />
+        </>
+    )
+}
