acc6786b40499e4de70c52f102d44bcf98cd8703
Mariana Martinez Araque <marianadelos.martinez@unet.edu.ve>
2023-02-13 11:16:36 -0400

    video 126. useFetch + useCounter
    para controlar los datos obtenidos con el api en un arreglo, se puede desestructurar la data a traves de un condicional con la doble negacion !!data && data[0]
    
    a traves de una expresion con un ternario se esta controlando si esta cargando o no la pagina

--- 05-hook-app/src/03-examples/MultipleCustomHooks.jsx
+++ 05-hook-app/src/03-examples/MultipleCustomHooks.jsx
@@ -4,14 +4,39 @@ import { useFetch } from "../hooks/useFetch";
 export const MultipleCustomHooks = () => {
 
     const {data,isLoading,hasError} = useFetch('https://api.breakingbadquotes.xyz/v1/quotes/1');
+    const {author, quote} = !!data && data[0];
 
     console.log({data,isLoading,hasError});
+
+    // if(isLoading){
+    //   return(
+    //     <h1>Cargando...</h1>
+    //   )
+    // }
+
+    // //no pueden colocarse hooks 
 
   return (
     <>
         <h1>BreakingBad Quotes</h1>
         <hr />
-    
+
+        {
+          isLoading ? (
+            <div className="alert alert-info text-center">
+              Loading...
+            </div>
+          ): 
+          (
+            <blockquote className="blockquote text-end"> 
+            <p className="mb-1">{quote}</p>
+            <footer className="blockquote-footer mb-1">{author}</footer>
+            </blockquote>
+          )
+        }
+
+        <button className="btn btn-primary">Next Quote</button>
+
     </>
   )
 }
