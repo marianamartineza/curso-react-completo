1dc4dc7a74d4773ee24799c2f157643eb1a94c93
Mariana Martinez Araque <marianadelos.martinez@unet.edu.ve>
2023-02-13 14:44:12 -0400

    video 131. memo - metodo de react
    es recomendable usarlo cuando se tienen componentes internos con funciones muy pesadas como por decir peticiones http para que no se redibuje cada vez que se realiza un cambio en el componente padre, en cambio se realice solo cuando hay un cambio en el valor del componente memorizandolo

---
+++ 05-hook-app/src/06-memos/Small.jsx
@@ -0,0 +1,11 @@
+// import {memo} from 'react';
+import React from 'react';
+
+export const Small = React.memo(({value}) => {
+
+    console.log('me volvi a dibujar :(');
+    
+  return (
+        <small>{value}</small>
+  )
+})
--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Memorize } from "./06-memos/Memorize";
 
 
 // import { HooksApp } from "./HooksApp";
@@ -9,13 +10,13 @@ import ReactDOM from "react-dom/client";
 // import { FormWithCustomHook } from "./02-useEffect/FormWithCustomHook";
 // import { MultipleCustomHooks } from "./03-examples/MultipleCustomHooks";
 // import { FocusScreen } from "./04-useRef/FocusScreen";
-import { Layout } from "./05-useLayoutEffect/Layout";
+// import { Layout } from "./05-useLayoutEffect/Layout";
 
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
-    <Layout/>
+    <Memorize/>
   //</React.StrictMode> 
 );
  
