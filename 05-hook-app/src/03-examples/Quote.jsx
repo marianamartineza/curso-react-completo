cb66656f26ed12ec95b594777da2fb5c6374ff75
Mariana Martinez Araque <marianadelos.martinez@unet.edu.ve>
2023-02-13 12:25:07 -0400

    video 130. useLayoutEffect
    variacion del useEffect que se realiza en paralelo con la renderizacion del componente.
    
    la funcion getBoundingClientRect() regresa el tamaño del elemento

--- 05-hook-app/src/03-examples/Quote.jsx
+++ 05-hook-app/src/03-examples/Quote.jsx
@@ -1,11 +1,35 @@
 import PropTypes from 'prop-types';
+import { useLayoutEffect, useRef, useState } from 'react';
 
 export const Quote = ({author,quote}) => {
+
+    const pRef = useRef();
+    
+    const [boxSize,setBoxSize] = useState({width:0, height:0});
+
+    useLayoutEffect(() => {
+        // console.log(pRef.current.getBoundingClientRect());
+        const {width,height} = pRef.current.getBoundingClientRect();
+        setBoxSize({width,height});
+    }, [quote])
+
   return (
-    <blockquote className="blockquote text-end"> 
-        <p className="mb-1">{quote}</p>
-         <footer className="blockquote-footer mb-1">{author}</footer>
-    </blockquote>
+    <>
+        <blockquote 
+            className="blockquote text-end"
+            style={{display: 'flex'}}
+        > 
+            <p 
+                className="mb-1"
+                ref={pRef}
+            >
+                    {quote}
+            </p>
+            <footer className="blockquote-footer mb-1">{author}</footer>
+        </blockquote>
+
+        <h5>{JSON.stringify(boxSize)}</h5>
+    </>
   )
 }
 
--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -1,20 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+
 // import { HooksApp } from "./HooksApp";
 // import { CounterApp } from "./01-useState/CounterApp";
 // import { CounterWithCustomHook } from "./01-useState/CounterWithCustomHook";
 // import { SimpleForm } from "./02-useEffect/SimpleForm";
 // import { FormWithCustomHook } from "./02-useEffect/FormWithCustomHook";
 // import { MultipleCustomHooks } from "./03-examples/MultipleCustomHooks";
-import { FocusScreen } from "./04-useRef/FocusScreen";
-
+// import { FocusScreen } from "./04-useRef/FocusScreen";
+import { Layout } from "./05-useLayoutEffect/Layout";
 
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
-    <FocusScreen/>
+    <Layout/>
   //</React.StrictMode> 
 );
  
