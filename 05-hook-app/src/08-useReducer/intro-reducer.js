5246785e2c73dff955441212a956c847c7929f66
Mariana Martinez Araque <marianadelos.martinez@unet.edu.ve>
2023-02-14 14:50:07 -0400

    Video 145. Idea general del reducer - via codigo
    Un reducer debe originalmente conocer un estado original, inicial o anterior al qur se va a tener despues del cambio en el estado.
    
    No se muta el estado directamente no se puede usar el push, se recomienda usar el ... para crear una copia del estado anterior e incluirle el nuevo elemento que uno quiera.

--- 05-hook-app/src/08-useReducer/intro-reducer.js
+++ 05-hook-app/src/08-useReducer/intro-reducer.js
@@ -1,1 +1,38 @@
-console.log('Hola Mundo');
+
+const initialState = [
+    {
+        id: 1,
+        todo: 'comprar queso',
+        done: false
+    }
+];
+
+const todoReducer = ( state = initialState, action = {}) => {
+
+    //regresar un nuevo estado
+    if(action.type === '[TODO] add todo'){
+        //no se puede mutar, la forma correcta es usar el spread para heredar sus propiedades ...
+        return [...state, action.payload];
+    }
+
+
+
+    return state;
+}
+
+let todos = todoReducer();
+
+const newTodo = {
+    id: 2,
+    todo: 'comprar pan',
+    done:false
+};
+
+const addTodoAction = {
+    type: '[TODO] add todo', //string sepa cual es la accion correpondiente
+    payload: newTodo //carga de cambio? 
+}
+
+todos = todoReducer( todos, addTodoAction );
+
+console.log({state: todos});
