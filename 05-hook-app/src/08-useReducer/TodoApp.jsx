5cc5ddaf1915b50788d1fa90305193e97167166f
Mariana Martinez Araque <marianadelos.martinez@unet.edu.ve>
2023-02-17 16:12:34 -0400

    video 150. tarea useTodo

--- 05-hook-app/src/08-useReducer/TodoApp.jsx
+++ 05-hook-app/src/08-useReducer/TodoApp.jsx
@@ -1,66 +1,13 @@
-import { useEffect, useReducer } from "react";
+import { useTodo } from "../hooks/useTodo";
 import { TodoAdd } from "./TodoAdd";
 import { TodoList } from "./TodoList";
-import { todoReducer } from "./todoReducer";
 
-//preferible usar la funcion initializer en el useReducer 
-const initialState = [
-    // {
-    //     id: new Date().getTime(),
-    //     description: 'Recolectar la piedra del alma',
-    //     done: false,
-    // },
-];
 
-//funcion inicializar el reducer a partir del almacenamiento local
-const init = () => {
-    return JSON.parse(localStorage.getItem('todos')) || [];
-}
 
 //componente react completo
 export const TodoApp = () => {
 
-    //hook useReducer
-    const [todos, dispatch] = useReducer(todoReducer, initialState, init);
-
-    //cuando los todos cambian hay q ejecutar un  efecto secundario 
-    useEffect(() => {
-      localStorage.setItem('todos',JSON.stringify(todos));
-    }, [todos])
-    
-
-    //acciones especificas del useReducer que se usan para controlarlo enviandolo a traves del dispatch
-
-    //guardar un nuevo todo
-    const handleNewTodo = (todo) => {
-
-        // console.log({todo});
-        const action = {
-            type: '[TODO] Add Todo',
-            payload: todo
-        }
-
-        dispatch(action);
-    }
-
-    //borrar un todo
-    const handleDeleteTodo = (id) => {
-        // console.log({id});
-        dispatch({
-            type: '[TODO] Remove Todo',
-            payload: id
-        });
-    }
-
-    //al hacer click marcar como completada el todo
-    const handleToggleTodo = (id) => {
-        // console.log({id})
-        dispatch({
-            type: '[TODO] Toggle Todo',
-            payload: id
-        })
-        
-    }
+    const {todos, handleDeleteTodo, handleNewTodo, handleToggleTodo} = useTodo();
 
     //las llamadas  a las funciones que controlan el reducer se hacen mediante props
     return (
---
+++ 05-hook-app/src/hooks/useTodo.js
@@ -0,0 +1,61 @@
+import { useEffect, useReducer } from "react";
+import { todoReducer } from "../08-useReducer/todoReducer";
+
+const init = () => {
+    return JSON.parse(localStorage.getItem('todos')) || [];
+}
+
+export const useTodo = () => {
+
+    //hook useReducer
+    const [todos, dispatch] = useReducer(todoReducer, [], init);
+
+    //cuando los todos cambian hay q ejecutar un  efecto secundario 
+    useEffect(() => {
+      localStorage.setItem('todos',JSON.stringify(todos));
+    }, [todos]);
+    
+
+    //acciones especificas del useReducer que se usan para controlarlo enviandolo a traves del dispatch
+
+    //guardar un nuevo todo
+    const handleNewTodo = (todo) => {
+
+        // console.log({todo});
+        const action = {
+            type: '[TODO] Add Todo',
+            payload: todo
+        }
+
+        dispatch(action);
+    }
+
+    //borrar un todo
+    const handleDeleteTodo = (id) => {
+        // console.log({id});
+        dispatch({
+            type: '[TODO] Remove Todo',
+            payload: id
+        });
+    }
+
+    //al hacer click marcar como completada el todo
+    const handleToggleTodo = (id) => {
+        // console.log({id})
+        dispatch({
+            type: '[TODO] Toggle Todo',
+            payload: id
+        })
+        
+    }
+
+    
+
+    return {
+        todos,
+        handleNewTodo,
+        handleDeleteTodo,
+        handleToggleTodo
+    }
+
+}
